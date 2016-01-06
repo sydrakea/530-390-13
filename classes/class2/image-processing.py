be39b0fa70e7ce9687d0bdbd2dd062e8b1ee0f18
Sydrake Abdi <sabdi1@jhu.edu>
2016-01-06 16:22:00 -0500

    test commit

--- classes/class2/image-processing.py
+++ classes/class2/image-processing.py
@@ -9,23 +9,23 @@ img = improc.read("gilman-hall.jpg")
 #img_nored = improc.rm_red(img)
 
 # convert to gray
-#img_gray_avg = improc.rgb_to_gray_avg(img)
+img_gray_avg = improc.rgb_to_gray_avg(img)
 #img_gray_lum = improc.rgb_to_gray_lum(img)
 
 #bigger = improc.scale(img,1.5)
 #smaller = improc.scale(bigger,2/3)
 #diff = improc.difference(smaller,img)
 
-#improc.show_n([img,smaller,diff])
+improc.show_n([img,img_gray_avg])
 
-n = 500
-A = np.random.rand(n)
-A2 = np.array(A)
-t0 = time.time()
-improc.selectionsort(A,n)
-t1 = time.time()
-improc.mergesort(A2,n)
-t2 = time.time()
-print(t1-t0, t2-t1)
+#n = 500
+#A = np.random.rand(n)
+#A2 = np.array(A)
+#t0 = time.time()
+#improc.selectionsort(A,n)
+#t1 = time.time()
+#improc.mergesort(A2,n)
+#t2 = time.time()
+#print(t1-t0, t2-t1)
 
 # I ADDED THIS LINE
